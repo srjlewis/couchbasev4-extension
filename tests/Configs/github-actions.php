e4cf9fdb7857ce3c8080a2f12c15fb40a3af24ad
steven.lewis <stevenlewis@gowebprint.com>
2024-01-07 11:22:00 +0000

    Remove closure wrapper

--- tests/Configs/github-actions.php
+++ tests/Configs/github-actions.php
@@ -1,11 +1,11 @@
 <?php
 use Phpfastcache\Drivers\Couchbasev4\Config as Couchbasev4Config;
 
-return (static fn(Couchbasev4Config $config) => $config->setItemDetailedDate(true)
+return (new Couchbasev4Config())
+    ->setItemDetailedDate(true)
     ->setUsername('test')
     ->setPassword('phpfastcache')
     ->setServers(['127.0.0.1'])
     ->setBucketName('phpfastcache')
     ->setScopeName('_default')
-    ->setCollectionName('_default')
-)(new Couchbasev4Config());
+    ->setCollectionName('_default');
