70dceff43605e2e77a0e669a43820d2e9fa18b2c
Georges <contact@geolim4.com>
2024-01-07 03:41:39 +0100

    Added semver compatibility tweak

--- lib/ExtensionRegister.php
+++ lib/ExtensionRegister.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Phpfastcache;
 
-use Phpfastcache\Extensions\Drivers\Couchbasev4\Driver;
+use Phpfastcache\Extensions\Drivers\Couchbasev4\{Config, Driver, Item};
+
+// Semver Compatibility until v10
+class_alias(Config::class, Drivers\Couchbasev4\Config::class);
+class_alias(Driver::class, Drivers\Couchbasev4\Driver::class);
+class_alias(Item::class, Drivers\Couchbasev4\Item::class);
 
 ExtensionManager::registerExtension('Couchbasev4', Driver::class);
